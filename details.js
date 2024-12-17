09f1482573946734a8901932f64a0f8b635ace95
tazin407 <tazintasnim34@gmail.com>
2024-12-17 19:21:14 +0600

    added preventDefault to like

--- details.js
+++ details.js
@@ -28,7 +28,7 @@ const loadDetails=()=>{
             
             likeParent= document.getElementById("likes");
             likeParent.innerHTML=`
-             <button onclick=postlike(${data.id}) class="btn">
+             <button onclick=postlike(${data.id}, event) class="btn">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
                 Like
               </button><br>
@@ -40,7 +40,8 @@ const loadDetails=()=>{
 };
 
 loadDetails();
-const postlike=(id)=>{
+const postlike=(id,event)=>{
+    event.preventDefault();
     var user_param= localStorage.getItem("user_id");
     info={
         id,
